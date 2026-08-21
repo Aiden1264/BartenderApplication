29704dce5102dd8c11878f71bc123703c7e26d62
IRONBOT3 <49288453+IRONBOT3@users.noreply.github.com>
2026-08-21 15:45:58 -0400

    Created CocktailOrder model.

---
+++ Models/CocktailOrder.cs
@@ -0,0 +1,24 @@
+﻿namespace BartenderApplication.Models
+{
+    public class CocktailOrder
+    {
+        public int Id { get; set; }
+
+        public string PatronName { get; set; }
+
+        public string CocktailName { get; set; }
+
+        public DateTime OrderTime { get; set; } = DateTime.Now;
+
+        public OrderStatus Status { get; set; } = OrderStatus.Queued;
+
+        public string Notes { get; set; }
+    }
+
+    public enum OrderStatus
+    {
+        Queued,
+        Preparing,
+        ReadyForPickup
+    }
+}
